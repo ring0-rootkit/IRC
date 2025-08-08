b44e8392b625b43302a5eef8b320982c57e4ad00
0x44 <83523587+CPScript@users.noreply.github.com>
2025-08-08 02:53:09 -0400

    Update irc.c

--- src/irc.c
+++ src/irc.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/socket.h>
 #include "irc.h"
 #include "network.h"
 #include "utils.h"
@@ -16,6 +17,7 @@ void send_command(const char* cmd) {
 }
 
 void* handle_input(void* arg) {
+    (void)arg;
     char buf[256];
     char cmdbuf[MAX_MSG_LENGTH];
 
@@ -25,11 +27,58 @@ void* handle_input(void* arg) {
             continue;
         }
 
+        // Strip newline character
+        buf[strcspn(buf, "\n")] = '\0';
+
+        // Skip empty messages
+        if (strlen(buf) == 0) {
+            continue;
+        }
+
+        // Handle IRC commands starting with '/'
+        if (buf[0] == '/') {
+            if (strncmp(buf, "/quit", 5) == 0) {
+                send_command("QUIT :Leaving\r\n");
+                break;
+            } else if (strncmp(buf, "/join ", 6) == 0) {
+                snprintf(cmdbuf, sizeof(cmdbuf), "JOIN %s\r\n", buf + 6);
+                send_command(cmdbuf);
+                continue;
+            }
+        }
+
         snprintf(cmdbuf, sizeof(cmdbuf), "PRIVMSG #%s :%s\r\n", config.channel, buf);
         send_command(cmdbuf);
-
-        log_info("Message sent.");
     }
+    
+    return NULL;
+}
+
+void* handle_server_response(void* arg) {
+    (void)arg;
+    char buffer[MAX_MSG_LENGTH];
+    int bytes_received;
+    
+    while ((bytes_received = recv(config.sockfd, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[bytes_received] = '\0';
+        
+        // Handle PING messages - IRC servers require PONG responses
+        if (strncmp(buffer, "PING", 4) == 0) {
+            char pong[MAX_MSG_LENGTH];
+            snprintf(pong, sizeof(pong), "PONG%s", buffer + 4);
+            send_command(pong);
+            log_info("Responded to server PING");
+        } else {
+            // Print server messages to stdout
+            printf("%s", buffer);
+            fflush(stdout);
+        }
+    }
+    
+    if (bytes_received <= 0) {
+        log_error("Server connection lost.");
+    }
+    
     return NULL;
 }
 
@@ -42,9 +91,13 @@ int send_initial_commands(const char* nick, const char* channel) {
     snprintf(cmdbuf, sizeof(cmdbuf), "USER %s 0 * :%s\r\n", nick, nick);
     send_command(cmdbuf);
 
+    // Small delay to allow registration
+    usleep(100000); // 100ms
+
     snprintf(cmdbuf, sizeof(cmdbuf), "JOIN #%s\r\n", channel);
     send_command(cmdbuf);
 
+    log_info("Initial IRC commands sent.");
     return 0;
 }
 
@@ -57,28 +110,56 @@ int startup(const char* server, const char* nick, const char* channel) {
 
     send_initial_commands(nick, channel);
 
-    pthread_create(&config.input_thread, NULL, handle_input, NULL);
+    // Create thread to handle server responses
+    if (pthread_create(&config.server_thread, NULL, handle_server_response, NULL) != 0) {
+        log_error("Failed to create server response thread.");
+        close(config.sockfd);
+        return 1;
+    }
 
+    // Create thread to handle user input
+    if (pthread_create(&config.input_thread, NULL, handle_input, NULL) != 0) {
+        log_error("Failed to create input thread.");
+        pthread_cancel(config.server_thread);
+        close(config.sockfd);
+        return 1;
+    }
+
+    log_info("IRC client started successfully.");
     return 0;
 }
 
 int parse_args(int argc, char** argv, irc_config_t* config) {
     int opt;
+    int server_set = 0, nick_set = 0, channel_set = 0;
+
     while ((opt = getopt(argc, argv, "s:n:c:")) != -1) {
         switch (opt) {
             case 's':
-                strncpy(config->server, optarg, INET6_ADDRSTRLEN);
+                strncpy(config->server, optarg, INET6_ADDRSTRLEN - 1);
+                config->server[INET6_ADDRSTRLEN - 1] = '\0';
+                server_set = 1;
                 break;
             case 'n':
-                strncpy(config->nick, optarg, MAX_NICK_LENGTH);
+                strncpy(config->nick, optarg, MAX_NICK_LENGTH - 1);
+                config->nick[MAX_NICK_LENGTH - 1] = '\0';
+                nick_set = 1;
                 break;
             case 'c':
-                strncpy(config->channel, optarg, MAX_CHANNEL_LENGTH);
+                strncpy(config->channel, optarg, MAX_CHANNEL_LENGTH - 1);
+                config->channel[MAX_CHANNEL_LENGTH - 1] = '\0';
+                channel_set = 1;
                 break;
             default:
                 printf("Usage: %s -s <irc-server> -n <nick> -c <channel>\n", argv[0]);
                 return 1;
         }
+    }
+
+    if (!server_set || !nick_set || !channel_set) {
+        printf("Error: All options (-s, -n, -c) are required.\n");
+        printf("Usage: %s -s <irc-server> -n <nick> -c <channel>\n", argv[0]);
+        return 1;
     }
 
     return 0;
