7e170fcbec6671a8dd5ea1e7118b97418933f123
0x44 <83523587+CPScript@users.noreply.github.com>
2025-08-08 02:53:36 -0400

    Update irc.h

--- include/irc.h
+++ include/irc.h
@@ -1,8 +1,8 @@
 #ifndef IRC_H
 #define IRC_H
 
-#include <netinet/in.h>
 #include <pthread.h>
+#include <netinet/in.h>
 
 #define MAX_MSG_LENGTH 512
 #define MAX_NICK_LENGTH 32
@@ -14,15 +14,17 @@ typedef struct {
     char server[INET6_ADDRSTRLEN];
     int sockfd;
     pthread_t input_thread;
+    pthread_t server_thread;
 } irc_config_t;
 
 extern irc_config_t config;
 
 void send_command(const char* cmd);
-void handle_input(void* arg);
+void* handle_input(void* arg);
+void* handle_server_response(void* arg);
 int send_initial_commands(const char* nick, const char* channel);
 int startup(const char* server, const char* nick, const char* channel);
 int parse_args(int argc, char** argv, irc_config_t* config);
 void cleanup(int sig);
 
-#endif // IRC_H
+#endif
